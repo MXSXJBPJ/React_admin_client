0aaa8f406877933cbeb9ba49a11bdc4eeabda9d6
Bert <Bert.PJ.Bai@pccw.com>
2020-05-30 22:18:36 +0800

    Add login from.

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,8 +1,48 @@
 import  React,{ Component } from "react";
-import './login.less'
-import logo from './images/logo.png'
+import './login.less';
+import logo from './images/logo.png';
+import { Form, Input, Button } from 'antd';
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
 /*登录路由组件 */
 export default class Login extends Component{
+
+    formRef = React.createRef();
+
+  onGenderChange = value => {
+    this.formRef.current.setFieldsValue({
+      note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+    });
+  };
+
+  onFinish = values => {
+    console.log(values);
+  };
+
+  onReset = () => {
+    this.formRef.current.resetFields();
+  };
+
+  onFill = () => {
+    this.formRef.current.setFieldsValue({
+      note: 'Hello world!',
+      gender: 'male',
+    });
+  };
     render(){
         return(
             <div className="login">
@@ -12,7 +52,61 @@ export default class Login extends Component{
                 </header>
                 <section className="login-content">
                      <h2>用户登录</h2>
-                     <div>From组件标签</div>
+                     <Form {...layout} ref={this.formRef} name="control-ref" onFinish={this.onFinish}>
+        <Form.Item
+          name="UserName"
+          label="UserName"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="Password "
+          label="Password"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          noStyle
+          shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+        >
+          {({ getFieldValue }) =>
+            getFieldValue('gender') === 'other' ? (
+              <Form.Item
+                name="customizeGender"
+                label="Customize Gender"
+                rules={[
+                  {
+                    required: true,
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            ) : null
+          }
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+          <Button htmlType="button" onClick={this.onReset}>
+            Reset
+          </Button>
+          <Button type="link" htmlType="button" onClick={this.onFill}>
+            Fill form
+          </Button>
+        </Form.Item>
+      </Form>
                 </section>
             </div>
         )
